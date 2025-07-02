f704107069391b15c780ef7e6e77c574a6515c77
Marco Aurelio Hansen de Oliveira <SSNPETT18@firjan.com.br>
2025-07-02 19:28:18 -0300

    estiliza o form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from "./App.module.css";
 import { CardGrid } from "./components/CardGrid/CardGrid";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
+import { AddItemForm } from "./components/AddItemForm/AddItemForm";
 
 function App() {
   const [wishs, setWishs] = useState([]);
@@ -11,13 +12,6 @@ function App() {
     description: "",
     urlImage: "",
   });
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
   const handleSubmit = (e) => {
     e.preventDefault();
     const updatedWishs = [...wishs, form];
@@ -28,41 +22,15 @@ function App() {
       urlImage: "",
     });
   };
-  console.log(wishs);
   return (
     <div className={styles.app}>
       <Header />
       <main className={styles.main}>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Desejo</label>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="description">Descrição</label>
-            <input
-              type="text"
-              name="description"
-              value={form.description}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="">URL Imagem</label>
-            <input
-              type="text"
-              name="urlImage"
-              value={form.urlImage}
-              onChange={handleChange}
-            />
-          </div>
-          <button type="submit">Adicionar</button>
-        </form>
+        <AddItemForm
+          handleSubmit={handleSubmit}
+          form={form}
+          setForm={setForm}
+        />
         <CardGrid wishs={wishs} />
       </main>
       <Footer />
